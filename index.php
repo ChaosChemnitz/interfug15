94ba6a3b2117f8920545477a428ed3e31fafa70b
/jdi/ <jdi@jadadev.de>
2015-07-21 23:50:47 +0200

    fancy responsiveness!

--- index.php
+++ index.php
@@ -23,39 +23,47 @@ Array("name"=>"kontakt","url"=>"/kontakt","title"=>"Kontakt")
 		<link rel="stylesheet" type="text/css" href="/css/style.css">
 	</head>
 	<body>
-		<table id="container" border="0" cellpadding="0" cellspacing="0">
-			<tr><td id="marx"></td>
-				<td id="main">
-					<header id="header">
-						<h1>INTERFUG #1//2015</h1>
-					</header>
-					<nav id="nav">
-						<ul>
-						<?php
-						foreach($pages as $p)
-							echo "<li><a href=\"".$p["url"]."\"".($pageId==$p["name"]?" class=\"active\"":"")."><span>".$p["title"]."</span></a></li>\n";
-						?>
-						</ul>
-					</nav>
-					<div>
-					<article>
-			<?php
-			foreach($pages as $p){
-				if($pageId==$p["name"]){
-					$c="if15".substr(base_convert(md5("if15".rand()), 16, 36), 0, 9);
-					echo str_replace("#code#",$c,file_get_contents("text/".$p["name"].".html"));
+		<div id="header" class="container">
+			<header id="title">
+				<h1>INTERFUG #1//2015</h1>
+			</header>
+			<nav id="nav">
+				<ul>
+				<?php
+				foreach($pages as $p)
+					echo "<li><a href=\"".$p["url"]."\"".($pageId==$p["name"]?" class=\"active\"":"")."><span>".$p["title"]."</span></a></li>\n";
+				?>
+				</ul>
+			</nav>
+			<div class="border">
+			</div>
+		</div>
+		<div id="main" class="container">	
+			<article id="content" class="box">
+				<div id="navplaceholder"></div>	
+				<?php
+				foreach($pages as $p){
+					if($pageId==$p["name"]){
+						$c="if15".substr(base_convert(md5("if15".rand()), 16, 36), 0, 9);
+						echo str_replace("#code#",$c,file_get_contents("text/".$p["name"].".html"));
+					}
 				}
-			}
-			?>
-					<!--footer id="footer" class="box" style="color:#C0C0C0">
-						<ul class="links">
-							<li><a href="https://chaoschemnitz.de/ChaosChemnitz:Impressum">Impressum</a></li>
-							<li>Design by ... + <a href="https://jdi.li/">/jdi/</a>.</li>
-						</ul>
-					</footer-->
-					</article>
-					</div>
-				</td><td id="line"></td>
+				/*
+				<!--footer id="footer" class="box" style="color:#C0C0C0">
+					<ul class="links">
+						<li><a href="https://chaoschemnitz.de/ChaosChemnitz:Impressum">Impressum</a></li>
+						<li>Design by ... + <a href="https://jdi.li/">/jdi/</a>.</li>
+					</ul>
+				</footer-->
+				*/
+				?>
+			</article>
+		</div>
+		<table id="container" border="0" cellpadding="0" cellspacing="0">
+			<tr>
+				<td id="marx"></td>
+				<td class="container"></td>
+				<td id="line"></td>
 			</tr>
 		</table>
 	</body>
